24fb83d2cfb4ebe80fb152e67651886c7f616bb7
Nathan M <nathanminissale@gmail.com>
2018-10-02 08:57:51 +0000

    automatically backed up by learn

--- draft.js
+++ draft.js
@@ -29,11 +29,16 @@ var cart = [{ itemName: 'bananas', itemPrice: 17 },
             { itemName: 'eggs', itemPrice: 49 }
            ];
 
-cart.forEach(function(element) {
+if (cart.forEach(function(element) {
                if (element.itemName == item) {
                  return true
                } else {
                  return false
                }
-             });
+             });) {
+    console.log(true);
+  
+} else {
+  console.log(false);
+}
 
