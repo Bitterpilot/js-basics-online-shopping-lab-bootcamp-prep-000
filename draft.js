c8640350d8eab7cd416d3b1a72e22b6759b824dc
Nathan M <nathanminissale@gmail.com>
2018-10-02 08:48:46 +0000

    automatically backed up by learn

--- draft.js
+++ draft.js
@@ -24,7 +24,7 @@
 // function removeFromCart
 var item = 'watermelon'
 
-var cart = [{ itemName: 'bananas', itemPrice: 17 },
+var cart = [//{ itemName: 'bananas', itemPrice: 17 },
             { itemName: 'watermelon', itemPrice: 5 },
             { itemName: 'eggs', itemPrice: 49 }
            ];
