f9e564e07f8ef70659a57ea2e4efbfd12ef53537
Nathan M <nathanminissale@gmail.com>
2018-10-02 09:28:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -84,5 +84,9 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber != undefined) {
+    
+  } else {
+    return `Sorry, we don't have a credit card on file for you.`
+  }
 }
