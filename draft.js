22dade69bdd63bcee1b13b78db86f012eb589a23
Nathan M <nathanminissale@gmail.com>
2018-10-02 08:42:18 +0000

    automatically backed up by learn

--- draft.js
+++ draft.js
@@ -23,17 +23,17 @@
 
 // function removeFromCart
 // var item = 'watermelon'
-// 
+//
 // var cart = [{ itemName: 'bananas', itemPrice: 17 },
 //             { itemName: 'watermelon', itemPrice: 5 },
 //             { itemName: 'eggs', itemPrice: 49 }
 //            ];
-// 
+//
 // console.log(cart.every(obj.itemName == item));
 // // cart.every(obj.itemName == item);
 console.log(
-[{a:1, b:2, c:3, d:4}, {a:1, x:2, y:3, z:4}, {a:1, x:2, y:3, z:4}].every(obj => obj.a === 1); //true
+[{a:1, b:2, c:3, d:4}, {a:1, x:2, y:3, z:4}, {a:1, x:2, y:3, z:4}].every(obj => obj.a === 1) //true
 );
 console.log(
-[{a:1, b:2, c:3, d:4}, {a:1, x:2, y:3, z:4}, {a:2, x:2, y:3, z:4}].every(obj => obj.a === 1); //false
+[{a:1, b:2, c:3, d:4}, {a:1, x:2, y:3, z:4}, {a:2, x:2, y:3, z:4}].every(obj => obj.a === 1) //false
 );
