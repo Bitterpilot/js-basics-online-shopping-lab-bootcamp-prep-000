226bad3937e19cbfa8c1f1a13237962f158f07be
Nathan M <nathanminissale@gmail.com>
2018-10-02 09:06:30 +0000

    automatically backed up by learn

--- draft.js
+++ draft.js
@@ -43,7 +43,7 @@ if (checkArray.includes(true)) {
   cart.forEach(function(element) {
     console.log(element.itemName);
     });
-  console.log(cart);
+  console.log(`true ${cart});
   return
 } else {
   console.log(`That item is not in your cart.`);
