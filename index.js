397319deb6e7b9beb353de3620f977489706168f
Nathan M <nathanminissale@gmail.com>
2018-10-02 09:28:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber != undefined) {
-    
+
   } else {
     return `Sorry, we don't have a credit card on file for you.`
   }
